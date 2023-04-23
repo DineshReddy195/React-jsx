4d95759275ec92dac1da44eef880842e118c2202
Dinesh Reddy <ndkr2716@gmail.com>
2023-04-23 10:54:05 +0530

    Assignment Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='card'>
+      <div>
+        <img src='https://pbs.twimg.com/profile_images/1421211957969960966/Vn7s-v3m_400x400.jpg' className='image' alt='pic'/>
+        <h2>Alexandra Caulea</h2>
+        <p>I enjoy drinking a cup of coffee everyday</p>
+      </div>
+      <div className='numbers'>
+        <p>172</p>
+        <p>47</p>
+        <p>20</p>
+      </div>
+      <div className='social'>
+        <h3>POSTS</h3>
+        <h3>FOLLOWERS</h3>
+        <h3>FOLLOWING</h3>
+      </div>
+      <div className='buttons'>
+        <button className='btn'>FOLLOW</button>
+        <button className='btn1'>MESSAGE</button>
+      </div>
+      </div>
     </div>
   );
 }
